832c7f756d365e3b4bb3dfdd08f8e62d5e4e693c
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-05-04 15:59:45 +0200

    Update Gas.R

--- Gas.R
+++ Gas.R
@@ -9,8 +9,8 @@ download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/n
 download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/nomine/2022/Nomine_202204_14-IT.xls", "Nomine_202204_14-IT.xls")
 download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/nomine/2022/Nomine_202205_14-IT.xls", "Nomine_202205_14-IT.xls")
 
-Nomine_gennaio_22 <- read_xls("C:/Users/Raffo/Downloads/Nomine_202201_14-IT.xls",1, range = "A14:AE44")
-Nomine_febbraio_22 <- read_xls("C:/Users/Raffo/Downloads/Nomine_202202_14-IT.xls",1, range = "A14:AE44")
+Nomine_gennaio_22 <- read_xls("Nomine_202201_14-IT.xls",1)
+Nomine_febbraio_22 <- read_xls("C:/Users/Raffo/Documents/Nomine_202202_14-IT.xls",1, range = "A14:AE44")
 Nomine_marzo_22 <- read_xls("C:/Users/Raffo/Downloads/Nomine_202203_14-IT.xls",1, range = "A14:AE44")
 Nomine_aprile_22 <- read_xls("C:/Users/Raffo/Downloads/Nomine_202204_14-IT.xls",1, range = "A14:AE44")
 Nomine_maggio_22 <- read_xls("C:/Users/Raffo/Downloads/Nomine_202205_14-IT.xls",1, range = "A14:AE44")
