47027a89ad27ffbc25211644e2f75759a3ee2caa
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-05-11 18:15:32 +0200

    Create calo_demografico.R

---
+++ calo_demografico.R
@@ -0,0 +1,6 @@
+library(devtools) 
+library(tidyverse)
+library(googlesheets4)
+library(readxl)
+
+fertilita_ue =read.csv("https://raw.githubusercontent.com/leopoldinho/Trasmissione-Sky/main/tps00199_page_linear_feritlita_09_20.csv")
