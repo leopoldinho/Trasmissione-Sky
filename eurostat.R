f02f5a5a31f79dec8a82a45f21f7bd75af2bcda9
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-05-11 18:05:55 +0200

    Update eurostat.R

--- eurostat.R
+++ eurostat.R
@@ -2,4 +2,5 @@ library(eurostat)
 tmp <- search_eurostat("mortality")
 head(tmp)
 
-fertility_ue = search_eurostat("fertility")
+fertility_ue_index = search_eurostat("fertility")
+fertility_ue=get_eurostat("TPS00199")
