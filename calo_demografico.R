f3d60d16be2a8f9f17183b5e685c2c200dccacf4
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-05-11 18:16:46 +0200

    Update calo_demografico.R

--- calo_demografico.R
+++ calo_demografico.R
@@ -11,3 +11,4 @@ fertility_ue_index = search_eurostat("fertility")
 fertilita_ue =read.csv("https://raw.githubusercontent.com/leopoldinho/Trasmissione-Sky/main/tps00199_page_linear_feritlita_09_20.csv")
 proiezioni_fertilita_paesi =get_eurostat("proj_19naasfr")
 proiezioni_fertilita_province =get_eurostat("proj_19raasfr3")
+fd
