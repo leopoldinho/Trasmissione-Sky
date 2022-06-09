918bddbab588de11eef1bbab8c3f61706ac22e18
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-06-09 17:04:30 +0200

    Update Inail.R

--- Inail.R
+++ Inail.R
@@ -1,11 +1,13 @@
 #Libraries
 
+
+
 library(devtools) 
 library(tidyverse)
 library(googlesheets4)
 library(httr)
 library(jsonlite)
-
+library(lubridate)
 
 #Scarico dati Abruzzo
 download.file("https://dati.inail.it/opendata/downloads/daticoncadenzamensileinfortuni/zip/DatiConCadenzaMensileInfortuniAbruzzo_csv.zip",
@@ -14,31 +16,34 @@ download.file("https://dati.inail.it/opendata/downloads/daticoncadenzasemestrale
               "abruzzo_sem.zip")
 
 
+#Sistemo il dataset ultimi due anni (compreso quello in corso)
+abruzzo_mese = read.csv2(unzip( "abruzzo.zip")) %>%
+  select(DataAccadimento, DataMorte, Genere, Eta, LuogoNascita, SettoreAttivitaEconomica)%>%
+  filter(DataMorte !="")
 
-abruzzo_mese = read.csv2(unzip( "abruzzo.zip"))
-abruzzo_mese$DataRilevazione <- 
-  as.Date(abruzzo_mese$DataRilevazione,format ="%d/%m/%Y")
-abruzzo_mese$DataProtocollo <- 
-  as.Date(abruzzo_mese$DataProtocollo,format ="%d/%m/%Y")
-abruzzo_mese$DataAccadimento <- 
+abruzzo_mese$DataAccadimento = 
   as.Date(abruzzo_mese$DataAccadimento,format ="%d/%m/%Y")
-abruzzo_mese$DataMorte <- 
+abruzzo_mese$DataMorte = 
   as.Date(abruzzo_mese$DataMorte,format ="%d/%m/%Y")
 
+#Sistemo il dataset 6 anni precedenti
+abruzzo_semest = read.csv2(unzip( "abruzzo_sem.zip")) %>%
+  select(DataAccadimento, DataMorte, Genere, Eta, LuogoNascita, SettoreAttivitaEconomica)%>%
+  filter(DataMorte !="")
 
-abruzzo_semest = read.csv2(unzip( "abruzzo_sem.zip"))
-abruzzo_semest$DataRilevazione <- 
-  as.Date(abruzzo_semest$DataRilevazione,format ="%d/%m/%Y")
-abruzzo_semest$DataProtocollo <- 
-  as.Date(abruzzo_semest$DataProtocollo,format ="%d/%m/%Y")
-abruzzo_semest$DataAccadimento <- 
+abruzzo_semest$DataAccadimento = 
   as.Date(abruzzo_semest$DataAccadimento,format ="%d/%m/%Y")
-abruzzo_semest$DataDefinizione <- 
-  as.Date(abruzzo_semest$DataDefinizione,format ="%d/%m/%Y")
-abruzzo_semest$DataMorte <- 
+abruzzo_semest$DataMorte = 
   as.Date(abruzzo_semest$DataMorte,format ="%d/%m/%Y")
 
+#unisco i dataset e calcolo andamento mensile
+abruzzo_morti_lavoro = bind_rows(abruzzo_semest,abruzzo_mese) %>%
+  mutate(decessi=as.numeric(1))%>% 
+  group_by(month = lubridate::floor_date(DataMorte, "month")) %>%
+  summarize(decessi_lavoro = sum(decessi)) %>%
+  rename(mese=month)
 
+#nb: provare a distinguera tra maschi e femmine
 
 
 #API
