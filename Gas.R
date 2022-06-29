028773cbf961f507b80651339e2aa37366393dc9
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-06-29 13:59:53 +0200

    Update Gas.R

--- Gas.R
+++ Gas.R
@@ -32,18 +32,18 @@ Sys.setenv(GIE_PAT = "9ad7312d3330ea12138bbc52e5461717")
 
 #Scarico i file dei bilanci del Gas
 
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202101-IT.xls", "bilancio_202101-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202102-IT.xls", "bilancio_202102-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202103-IT.xls", "bilancio_202103-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202104-IT.xls", "bilancio_202104-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202105-IT.xls", "bilancio_202105-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202106-IT.xls", "bilancio_202106-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202107-IT.xls", "bilancio_202107-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202108-IT.xls", "bilancio_202108-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202109-IT.xls", "bilancio_202109-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202110-IT.xls", "bilancio_202110-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202111-IT.xls", "bilancio_202111-IT.xls", mode="wb")
-download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202112-IT.xls", "bilancio_202112-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202101-IT.xls", "bilancio_202101-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202102-IT.xls", "bilancio_202102-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202103-IT.xls", "bilancio_202103-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202104-IT.xls", "bilancio_202104-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202105-IT.xls", "bilancio_202105-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202106-IT.xls", "bilancio_202106-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202107-IT.xls", "bilancio_202107-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202108-IT.xls", "bilancio_202108-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202109-IT.xls", "bilancio_202109-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202110-IT.xls", "bilancio_202110-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202111-IT.xls", "bilancio_202111-IT.xls", mode="wb")
+#download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2021/bilancio_202112-IT.xls", "bilancio_202112-IT.xls", mode="wb")
 download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2022/bilancio_202201-IT.xls", "Bilancio_202201-IT.xls", mode="wb")
 download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2022/bilancio_202202-IT.xls", "Bilancio_202202-IT.xls", mode="wb")
 download.file("https://www.snam.it/exchange/quantita_gas_trasportato/andamento/bilancio_definitivo/2022/bilancio_202203-IT.xls", "bilancio_202203-IT.xls", mode="wb")
@@ -62,55 +62,57 @@ download.file("https://github.com/leopoldinho/Trasmissione-Sky/blob/main/bilanci
 #2021
 
 
-Bilancio_gen_21 = read_xls("bilancio_202101-IT.xls",3,range = "A14:AE45") 
-Bilancio_gen_21$GG=as.Date(Bilancio_gen_21$GG <- paste0('2021-01-', Bilancio_gen_21$GG))
-Bilancio_feb_21 = read_excel("bilancio_202102-IT.xls",3,range = "A14:AE44")  %>%
-  slice_head(n = 28)
-Bilancio_feb_21$GG=as.Date(Bilancio_feb_21$GG <- paste0('2021-02-', Bilancio_feb_21$GG))
-Bilancio_mar_21 = read_excel("bilancio_202103-IT.xls",3,range = "A14:AE45") 
-Bilancio_mar_21$GG=as.Date(Bilancio_mar_21$GG <- paste0('2021-03-', Bilancio_mar_21$GG))
-Bilancio_apr_21 = read_excel("bilancio_202104-IT.xls",3,range = "A14:AE44") 
-Bilancio_apr_21$GG=as.Date(Bilancio_apr_21$GG <- paste0('2021-04-', Bilancio_apr_21$GG))
-Bilancio_mag_21 = read_excel("bilancio_202105-IT.xls",3,range = "A14:AE45") 
-Bilancio_mag_21$GG=as.Date(Bilancio_mag_21$GG <- paste0('2021-05-', Bilancio_mag_21$GG))
-Bilancio_giu_21 = read_excel("bilancio_202106-IT.xls",3,range = "A14:AE44") 
-Bilancio_giu_21$GG=as.Date(Bilancio_giu_21$GG <- paste0('2021-06-', Bilancio_giu_21$GG))
-Bilancio_lug_21 = read_excel("bilancio_202107-IT.xls",3,range = "A14:AE45") 
-Bilancio_lug_21$GG=as.Date(Bilancio_lug_21$GG <- paste0('2021-07-', Bilancio_lug_21$GG))
-Bilancio_ago_21 = read_excel("bilancio_202108-IT.xls",3,range = "A14:AE45") 
-Bilancio_ago_21$GG=as.Date(Bilancio_ago_21$GG <- paste0('2021-08-', Bilancio_ago_21$GG))
-Bilancio_set_21 = read_excel("bilancio_202109-IT.xls",3,range = "A14:AE44") 
-Bilancio_set_21$GG=as.Date(Bilancio_set_21$GG <- paste0('2021-09-', Bilancio_set_21$GG))
-Bilancio_ott_21 = read_excel("bilancio_202110-IT.xls",3,range = "A14:AE45") 
-Bilancio_ott_21$GG=as.Date(Bilancio_ott_21$GG <- paste0('2021-10-', Bilancio_ott_21$GG))
-Bilancio_nov_21 = read_excel("bilancio_202111-IT.xls",3,range = "A14:AE44") 
-Bilancio_nov_21$GG=as.Date(Bilancio_nov_21$GG <- paste0('2021-11-', Bilancio_nov_21$GG))
-Bilancio_dic_21 = read_excel("bilancio_202112-IT.xls",3,range = "A14:AE45") 
-Bilancio_dic_21$GG=as.Date(Bilancio_dic_21$GG <- paste0('2021-12-', Bilancio_dic_21$GG))
+#Bilancio_gen_21 = read_xls("bilancio_202101-IT.xls",3,range = "A14:AE45") 
+#Bilancio_gen_21$GG=as.Date(Bilancio_gen_21$GG <- paste0('2021-01-', Bilancio_gen_21$GG))
+#Bilancio_feb_21 = read_excel("bilancio_202102-IT.xls",3,range = "A14:AE44")  %>%
+#slice_head(n = 28)
+#Bilancio_feb_21$GG=as.Date(Bilancio_feb_21$GG <- paste0('2021-02-', Bilancio_feb_21$GG))
+##Bilancio_mar_21 = read_excel("bilancio_202103-IT.xls",3,range = "A14:AE45") 
+#Bilancio_mar_21$GG=as.Date(Bilancio_mar_21$GG <- paste0('2021-03-', Bilancio_mar_21$GG))
+#Bilancio_apr_21 = read_excel("bilancio_202104-IT.xls",3,range = "A14:AE44") 
+#Bilancio_apr_21$GG=as.Date(Bilancio_apr_21$GG <- paste0('2021-04-', Bilancio_apr_21$GG))
+#Bilancio_mag_21 = read_excel("bilancio_202105-IT.xls",3,range = "A14:AE45") 
+#Bilancio_mag_21$GG=as.Date(Bilancio_mag_21$GG <- paste0('2021-05-', Bilancio_mag_21$GG))
+#Bilancio_giu_21 = read_excel("bilancio_202106-IT.xls",3,range = "A14:AE44") 
+#Bilancio_giu_21$GG=as.Date(Bilancio_giu_21$GG <- paste0('2021-06-', Bilancio_giu_21$GG))
+#Bilancio_lug_21 = read_excel("bilancio_202107-IT.xls",3,range = "A14:AE45") 
+#Bilancio_lug_21$GG=as.Date(Bilancio_lug_21$GG <- paste0('2021-07-', Bilancio_lug_21$GG))
+#Bilancio_ago_21 = read_excel("bilancio_202108-IT.xls",3,range = "A14:AE45") 
+#Bilancio_ago_21$GG=as.Date(Bilancio_ago_21$GG <- paste0('2021-08-', Bilancio_ago_21$GG))
+#Bilancio_set_21 = read_excel("bilancio_202109-IT.xls",3,range = "A14:AE44") 
+#Bilancio_set_21$GG=as.Date(Bilancio_set_21$GG <- paste0('2021-09-', Bilancio_set_21$GG))
+#Bilancio_ott_21 = read_excel("bilancio_202110-IT.xls",3,range = "A14:AE45") 
+#Bilancio_ott_21$GG=as.Date(Bilancio_ott_21$GG <- paste0('2021-10-', Bilancio_ott_21$GG))
+#Bilancio_nov_21 = read_excel("bilancio_202111-IT.xls",3,range = "A14:AE44") 
+#Bilancio_nov_21$GG=as.Date(Bilancio_nov_21$GG <- paste0('2021-11-', Bilancio_nov_21$GG))
+#Bilancio_dic_21 = read_excel("bilancio_202112-IT.xls",3,range = "A14:AE45") 
+#Bilancio_dic_21$GG=as.Date(Bilancio_dic_21$GG <- paste0('2021-12-', Bilancio_dic_21$GG))
 
 #Unisco i file 2021
-Bilancio_Gas_2021 = bind_rows(Bilancio_gen_21,Bilancio_feb_21,Bilancio_mar_21,
-                              Bilancio_apr_21,Bilancio_mag_21,Bilancio_giu_21,
-                              Bilancio_lug_21,Bilancio_ago_21,Bilancio_set_21,
-                              Bilancio_ott_21,Bilancio_nov_21,Bilancio_dic_21
-                              )%>% 
-  select(GG, "2021"=Import., "Entrata Tarvisio 21"="Entrata Tarvisio",
-         "Entrata Gela 21"="Entrata Gela","Entrata Gorizia 21"="Entrata Gorizia",
-         "Entrata Mazara 21"="Entrata Mazara","Entrata P.Gries 21"="Entrata P.Gries",
-         "Entrata Melendugno 21"="Entrata Melendugno","GNL Cavarzere 21"="GNL Cavarzere",
-         "GNL Livorno 21"="GNL Livorno","GNL Panigaglia 21"="GNL Panigaglia",
-         "Prod Nazionale 21"="Produzione Nazionale","Sistemi di stoccaggio 21"="Sistemi di stoccaggio*") %>%
-  mutate(GNL_tot_21=Reduce("+",.[9:11]))
-  
+#Bilancio_Gas_2021 = bind_rows(Bilancio_gen_21,Bilancio_feb_21,Bilancio_mar_21,
+#                             Bilancio_apr_21,Bilancio_mag_21,Bilancio_giu_21,
+#                             Bilancio_lug_21,Bilancio_ago_21,Bilancio_set_21,
+#                             Bilancio_ott_21,Bilancio_nov_21,Bilancio_dic_21
+#                             )%>% 
+# select(GG, "2021"=Import., "Entrata Tarvisio 21"="Entrata Tarvisio",
+#        "Entrata Gela 21"="Entrata Gela","Entrata Gorizia 21"="Entrata Gorizia",
+#        "Entrata Mazara 21"="Entrata Mazara","Entrata P.Gries 21"="Entrata P.Gries",
+#        "Entrata Melendugno 21"="Entrata Melendugno","GNL Cavarzere 21"="GNL Cavarzere",
+#        "GNL Livorno 21"="GNL Livorno","GNL Panigaglia 21"="GNL Panigaglia",
+#        "Prod Nazionale 21"="Produzione Nazionale","Sistemi di stoccaggio 21"="Sistemi di stoccaggio*") %>%
+# mutate(GNL_tot_21=Reduce("+",.[9:11]))
+# 
 
 #Elaboro il conteggio per settimana 2021
-Bilancio_Gas_2021_Set =Bilancio_Gas_2021 %>% 
-  mutate(Settimana = cut.Date(GG, breaks = "1 week", labels = FALSE)) %>% 
-  arrange(GG) %>% select(-GG) %>% mutate_if(is.numeric, round, 1)
+#Bilancio_Gas_2021_Set =Bilancio_Gas_2021 %>% 
+# mutate(Settimana = cut.Date(GG, breaks = "1 week", labels = FALSE)) %>% 
+# arrange(GG) %>% select(-GG) %>% mutate_if(is.numeric, round, 1)
 
-Bilancio_Gas_2021_Set = Bilancio_Gas_2021_Set %>%
-  group_by(Settimana) %>% summarise_all(sum)
+#Bilancio_Gas_2021_Set = Bilancio_Gas_2021_Set %>%
+# group_by(Settimana) %>% summarise_all(sum)
 
+Bilancio_Gas_2021_Set = read.csv("https://raw.githubusercontent.com/leopoldinho/Trasmissione-Sky/main/bilancio_gas_2021_set.csv",
+                                 check.names = FALSE)
 
 #2022
 Bilancio_gennaio_22 = read_excel("Bilancio_202201_14-IT.xls",3,range = "A14:AE45") 
