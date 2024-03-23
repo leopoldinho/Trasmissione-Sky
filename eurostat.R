95bde85cf4d7af53882e34109f29c81a3d030ea2
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2024-03-23 15:15:27 +0100

    Update eurostat.R

--- eurostat.R
+++ eurostat.R
@@ -1,6 +1,12 @@
+install.packages("eurostat")
 library(eurostat)
 tmp <- search_eurostat("mortality")
 head(tmp)
 
+install.packages("rlang")
+
 fertility_ue_index = search_eurostat("fertility")
 fertility_ue=get_eurostat("demo_r_find3")
+
+
+emissioni_ue = get_eurostat("env_air_gge")
